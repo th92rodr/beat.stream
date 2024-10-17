2e14db51135300182c56d4195169947a78dd2724
th92rodr <thiago.clrodrigues@gmail.com>
2024-10-17 15:10:48 -0300

    configure postgresql db using prisma

--- server/src/env.ts
+++ server/src/env.ts
@@ -3,6 +3,8 @@ import { z } from 'zod'
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production']),
 
+  DATABASE_URL: z.string().url(),
+
   HOST: z.string().default('localhost'),
   PORT: z.coerce.number().default(3333),
   CONNECTION_TIMEOUT: z.coerce.number(),
